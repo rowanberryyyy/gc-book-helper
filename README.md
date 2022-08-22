9f0d9afa343a9178aac96bb1644a01662a8ffeca
Diyar Oktay <diyar.oktay@outlook.com>
2022-08-22 23:30:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+See the link `booksUrl` in [./index.user.js]() for the list of books
+
+First, go to JN database and find the books released between 1999 and 2002
+
+[https://items.jellyneo.net/search/?cat[]=5&min_rarity=1&max_rarity=499&sort=3&limit=75&max_release=04%2F18%2F2002&start=0]()
+
+ April 18, 2002 
+
+Now run this script once on each page
+```js
+const books = localStorage.getItem("book") ? JSON.parse(localStorage.getItem("book")) : []
+
+const ignore = ["All About Grey Faeries"]
+
+const onPage = Array.from(document.querySelectorAll(".item-result-image"))
+    .map((e) => {
+        // Example : Cool Amazing Book - r85
+        let [name,rarity] = e.title.split(" - r") 
+
+        // The search includes unreleased items
+        // although right now there is only 1
+        if (ignore.includes(name)) return -1
+        return [name, parseInt(rarity)]
+    })
+    .filter((e) => e !== -1)
+
+books.push(...onPage)
+
+localStorage.setItem("book", JSON.stringify(books))
+```
+
+When you are done, retrieve your result
+
+```js
+JSON.parse(localStorage.book)
+```
+
+
+Right click and "Copy object"
+
+The result should be an array with N items, where N is the number of books released (in this case 428). Each item should be an array of length 2 with the first item being the title of the book (string) and the second being the rarity (number)
+
+And clean up
+```js
+delete localStorage.book
+```
+
+Watch out for unreleased items. Their names should be added to the "ignore" array
+
+
+Known problem: Retired items are items which have their originial rarity changed to 180. In the JN export some items are marked as r180 because they retired on Neopets. But on Grundo's cafe they are still active. Frankly, I believe this script probably be a part of vanilla Grundo's cafe soon so let's see how effective it is to take the rarity changes into account now.
---
+++ index.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Grundo's Cafe Book Helper
+// @namespace    github.com/windupbird144/
+// @version      0.1
+// @description  Show unread books on your pet's book page
+// @author       You
+// @match        https://www.grundos.cafe/books_read/?pet_name=*
+// @icon         https://www.grundos.cafe/static/images/favicon.66a6c5f11278.ico
+// @grant        none
+// ==/UserScript==
+
+const booksUrl = `https://gist.githubusercontent.com/windupbird144/7eb8f24ae3c64feb095edf8935305458/raw/d0ee389b14e2c83f30f1c2ead78d60787aab2711/books.json`
+
+/**
+ * Analyse the document to find out which books your pet has read
+ * 
+ * @returns string[] title of book your pets has read
+ */
+const getReadBooks = () => 
+    Array.from(document.querySelectorAll("#center > table > tbody > tr:nth-of-type(n+2)")).map(e => e.childNodes[3].childNodes[0].textContent.trim().replace(" (",""))
+
+/**
+ * Return a list of books your pet has not read
+ * TODO: Use a more efficient algorithm
+ * 
+ * @param {[string, number][]} listOfBooks 
+ * @param {string[]} booksRead
+ * 
+ * @returns {[string, number][]} the books your pet has not read 
+ */
+const filterReadBooks = (listOfBooks, booksRead) => {
+    return listOfBooks.filter(([name,_]) => !booksRead.includes(name))
+}
+
+
+const html = (books) => `<div class="books-to-read" style="padding-left: 2em;">
+<h3 style="text-align: center">Your pet has ${books.length} books left read!</h3>
+<ul style="columns: 2">
+${
+    books.map(([name, rarity]) => `<li><span style="user-select:all">${name}</span> (<b>r${rarity})</b></li>`).join("")
+}
+</ul>
+</div>`;
+
+async function main() {
+    const listOfBooks = await fetch(booksUrl).then(res => res.json())
+    const toRead = filterReadBooks(listOfBooks, getReadBooks())
+    document.querySelector(".content").insertAdjacentHTML("afterend", html(toRead))
+}
+
+main().then()
