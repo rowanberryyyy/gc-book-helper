c021d88236e2502a420ed0d6f103e5b4fe617eae
Rowan <110063671+rowanberryyyy@users.noreply.github.com>
2024-07-01 18:15:02 +0100

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Grundo's Cafe Book Helper
 // @namespace    github.com/windupbird144/
-// @version      0.2
+// @version      0.4
 // @description  Show unread books on your pet's book page
 // @author       You
 // @match        https://www.grundos.cafe/books_read/?pet_name=*
@@ -18,7 +18,7 @@ const booksUrl = `https://raw.githubusercontent.com/rowanberryyyy/gc-book-helper
  * @returns string[] title of book your pets has read
  */
 const getReadBooks = () => 
-    Array.from(document.querySelectorAll(".center > table > tbody > tr:nth-of-type(n+2)")).map(e => e.childNodes[3].childNodes[0].textContent.trim().replace(" (",""))
+    Array.from(document.querySelectorAll(".center > table > tbody > tr:nth-of-type(n+2)")).map(e => e.childNodes[3].childNodes[0].textContent.trim().replace(" (","").toUpperCase())
 
 /**
  * Return a list of books your pet has not read
@@ -30,23 +30,31 @@ const getReadBooks = () =>
  * @returns {[string, number][]} the books your pet has not read 
  */
 const filterReadBooks = (listOfBooks, booksRead) => {
-    return listOfBooks.filter(([name,_]) => !booksRead.includes(name))
+    return listOfBooks.filter(([name,_]) => !booksRead.includes(name.toUpperCase()))
 }
 
 
 // callback to sort books by rarity by rarity
 const byRarity = ([_,rarity1],[__,rarity2]) => rarity1 - rarity2
 
+// One liner function:
+const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
+
+// Usage:
+addCSS(".booklist { height: 80px; overflow: auto;} .booklist:hover { height: 500px; overflow: none;")
+
 const html = (books) => `<div class="books-to-read" style="line-height: 20px; padding-left: 2em;">
 <h3 style="text-align: center">Your pet has ${books.length} books left to read!</h3>
-<ul style="columns: 2">
+<h4 style="text-align: center">(hover to expand)</h3>
+<div class="booklist"><ul style="columns: 2">
 ${
     books.sort(byRarity).map(([name, rarity]) => `<li><span style="user-select:all">${name}</span> (<b>r${rarity})</b>
     <a href="/market/wizard/?query=${name}" target="_bookSearch"><img width="15px" src="https://s3.us-west-2.amazonaws.com/cdn.grundos.cafe/misc/wiz.png"></a>
     <a href="/island/tradingpost/browse/?query=${name}" target="_bookSearch"><img width="15px" src="https://s3.us-west-2.amazonaws.com/cdn.grundos.cafe/misc/tp.png"></a>
+    <a href="/safetydeposit/?page=1&category=&type=&query=${name}" target="_bookSearch"><img width="15px" src="https://s3.us-west-2.amazonaws.com/cdn.grundos.cafe/misc/sdb.gif"></a>
     </li>`).join("")
 }
-</ul>
+</ul></div>
 </div>`;
 
 //Ty Riz for the SW/TP add-on!
@@ -54,7 +62,7 @@ ${
 async function main() {
     const listOfBooks = await fetch(booksUrl).then(res => res.json())
     const toRead = filterReadBooks(listOfBooks, getReadBooks())
-    document.querySelector(".content").insertAdjacentHTML("afterend", html(toRead))
+    document.querySelector(".content").insertAdjacentHTML("beforebegin", html(toRead))
 }
 
 main().then()
